6814a0d25662dbcd7f99d1699be5107d5b5628c9
HarshitaBhamare <harshitabhamare16@gmail.com>
2024-08-11 22:53:05 +0530

    almost donr

--- Arrays/FindTheClosestZero.java
+++ Arrays/FindTheClosestZero.java
@@ -1,11 +1,27 @@
 public class FindTheClosestZero {
     public static void main(String[] args) {
-        int[] nums = { -4, -2, 1, 4, 8 };
+        int[] nums = { -1000, -1000 };
         System.out.println(findClosestNumber(nums));
     }
 
     public static int findClosestNumber(int[] nums) {
-
-        return -1;
+        // int[] n = new int[nums.length];
+        int n = 0;
+        int m = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= 0) {
+                n = Math.abs(nums[i]);
+                nums[i] = n;
+            }
+            System.out.println(nums[i]);
+            int min = nums[0];
+            System.out.println(min);
+            if (nums[i] < min) {
+                min = nums[i];
+                m = min;
+                System.out.println("m" + min);
+            }
+        }
+        return m;
     }
 }
